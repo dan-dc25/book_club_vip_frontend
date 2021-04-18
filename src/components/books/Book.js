c4ddac35ad276944b01a82a77a2720def54227e5
Daniel <danielcarvalhovr@hotmail.com>
2021-04-18 17:24:40 -0400

    updated the book component

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -4,7 +4,60 @@ import NotesContainer from '../../containers/NotesContainer';
 
 class Book extends React.Component {
     
-   
+    findBook() {
+        let id = parseInt(this.props.match.params.id, 10);
+
+        let book = this.props.books.find(book => book.id === id);
+
+        return book;
+    }
+
+    handleAddBook(e) {
+        let book = this.findBook();
+        let configObj = {
+            user_id: this.props.user.id,
+            book_id: book.id
+        }
+      
+        this.props.addUserBook(configObj);
+    }
+
+    handleRemoveBook(e) {
+        let book = this.findBook();
+          
+        this.props.removeUserBook(this.props.user.id, book.id);
+    }
+    
+    renderButton() {
+        let book = this.findBook();
+
+        let foundBook = this.props.user.userBooks.find(b => b.id === book.id)
+
+        if (foundBook !== undefined) {        
+            return (
+            <button className="btn btn-primary" onClick={e => this.handleRemoveBook(e)}>Remove Book</button>
+            )
+        } else {
+            return (
+            <button className="btn btn-primary" onClick={e => this.handleAddBook(e)}>Add Book</button>
+            )
+        }  
+    }
+    
+    renderNotes() {
+        let book = this.findBook();
+
+        let foundBook = this.props.user.userBooks.find(b => b.id === book.id)
+
+        if (foundBook !== undefined) {        
+            return (
+                <NotesContainer book={book} user={this.props.user} />
+            )
+        } else {
+            return null
+        }
+    
+    }
 
     render() {
 
