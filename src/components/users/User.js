3fda3bab4f40a35cb6267e8df0d460def6ed76c0
Daniel <danielcarvalhovr@hotmail.com>
2021-04-18 15:09:19 -0400

    added another line to user container

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -7,7 +7,7 @@ const User = props => {
     return (
     <div className="container">
     
-        
+        <h4>{props.user.username}'s Books</h4>
         <div className="row row-cols-1 row-cols-md-4">
             {props.user.userBooks.map(book => <BookCard key={book.id} book={book} />)}
         </div>
